58e1a7babee796e9dfaa8e9a22c2607e9fda9290
Qingyun Wu <qingyun0327@gmail.com>
2024-08-25 23:39:19 -0700

    header wording

--- dotnet/src/AutoGen.Mistral/Extension/MistralAgentExtension.cs
+++ dotnet/src/AutoGen.Mistral/Extension/MistralAgentExtension.cs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Contributions to this project, i.e., https://github.com/autogen-ai/autogen, 
 // are licensed under the Apache License, Version 2.0 (Apache-2.0).
-// Portions derived from  https://github.com/microsoft/autogen under the MIT License, before release v0.2.35.
+// Portions derived from  https://github.com/microsoft/autogen under the MIT License..
 // SPDX-License-Identifier: MIT
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // MistralAgentExtension.cs

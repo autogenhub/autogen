fe3a627aaa00aa6a6877b91172f08c62c6126ffc
Mark Sze <mark@sze.family>
2024-08-30 07:42:06 +0000

    Populating agent messages completely in resume

--- autogen/agentchat/groupchat.py
+++ autogen/agentchat/groupchat.py
@@ -1270,11 +1270,15 @@ class GroupChatManager(ConversableAgent):
             if not message_speaker_agent and message["name"] == self.name:
                 message_speaker_agent = self
 
-            # Add previous messages to each agent (except their own messages and the last message, as we'll kick off the conversation with it)
+            # Add previous messages to each agent (except the last message, as we'll kick off the conversation with it)
             if i != len(messages) - 1:
                 for agent in self._groupchat.agents:
-                    if agent.name != message["name"]:
-                        self.send(message, self._groupchat.agent_by_name(agent.name), request_reply=False, silent=True)
+                    if agent.name == message["name"]:
+                        # An agent`s message is sent to the Group Chat Manager
+                        agent.send(message, self, request_reply=False, silent=True)
+                    else:
+                        # Otherwise, messages are sent from the Group Chat Manager to the agent
+                        self.send(message, agent, request_reply=False, silent=True)
 
                 # Add previous message to the new groupchat, if it's an admin message the name may not match so add the message directly
                 if message_speaker_agent:

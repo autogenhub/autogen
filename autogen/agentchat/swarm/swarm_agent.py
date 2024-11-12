3a6087d5bb445cde6034d614a3abb1fb32ea346b
Mark Sze <mark@sze.family>
2024-11-12 23:43:58 +0000

    Move swarm out of contrib, placeholder additions for swarm selection mode in groupchat

--- autogen/agentchat/swarm/swarm_agent.py
+++ autogen/agentchat/swarm/swarm_agent.py
@@ -1,27 +1,32 @@
-import autogen
-import os
 import json
+import os
 from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Type, TypeVar, Union
+
 from openai.types.chat.chat_completion import ChatCompletion
-from autogen import Agent, OpenAIWrapper, ConversableAgent
+
+import autogen
+from autogen.agentchat import Agent, ConversableAgent
 from autogen.function_utils import get_function_schema, load_basemodels_if_needed, serialize_to_str
+from autogen.oai import OpenAIWrapper
+
 
 def parse_json_object(response: str) -> dict:
     return json.loads(response)
 
 
 class SwarmAgent(ConversableAgent):
-    def __init__(self,
-                name: str,
-                system_message: Optional[str] = "You are a helpful AI Assistant.",
-                llm_config: Optional[Union[Dict, Literal[False]]] = None,
-                functions: Union[List[Callable], Callable]=None,
-                is_termination_msg: Optional[Callable[[Dict], bool]] = None,
-                max_consecutive_auto_reply: Optional[int] = None,
-                human_input_mode: Literal["ALWAYS", "NEVER", "TERMINATE"] = "NEVER",
-                description: Optional[str] = None,
-                **kwargs,
-                ) -> None:
+    def __init__(
+        self,
+        name: str,
+        system_message: Optional[str] = "You are a helpful AI Assistant.",
+        llm_config: Optional[Union[Dict, Literal[False]]] = None,
+        functions: Union[List[Callable], Callable] = None,
+        is_termination_msg: Optional[Callable[[Dict], bool]] = None,
+        max_consecutive_auto_reply: Optional[int] = None,
+        human_input_mode: Literal["ALWAYS", "NEVER", "TERMINATE"] = "NEVER",
+        description: Optional[str] = None,
+        **kwargs,
+    ) -> None:
         super().__init__(
             name,
             system_message,
@@ -42,15 +47,20 @@ class SwarmAgent(ConversableAgent):
 
     def update_context_variables(self, context_variables: Dict[str, Any]) -> None:
         pass
+
     # return str or any instance of BaseModel from pydantic
 
-    def generate_reply_with_tool_calls(        
-            self,
-            messages: Optional[List[Dict]] = None,
-            sender: Optional[Agent] = None,
-            config: Optional[OpenAIWrapper] = None,
+    def generate_reply_with_tool_calls(
+        self,
+        messages: Optional[List[Dict]] = None,
+        sender: Optional[Agent] = None,
+        config: Optional[OpenAIWrapper] = None,
     ) -> Tuple[bool, Union[str, Dict, None]]:
-        messages = self._oai_system_message + [{'role': 'user', 'content': input}]
+
+        if messages is None:
+            messages = self._oai_messages[sender]
+
+        messages = self._oai_system_message + [{"role": "user", "content": input}]
         response = self.client.create(messages=messages)
 
         if isinstance(response, ChatCompletion):
@@ -65,18 +75,16 @@ class SwarmAgent(ConversableAgent):
 
     def add_single_function(self, func: Callable, description=""):
         func._name = func.__name__
-        
+
         if description:
             func._description = description
         else:
             func._description = func.__doc__
-        
+
         f = get_function_schema(func, name=func._name, description=func._description)
         self.update_tool_signature(f, is_remove=False)
         self.register_function({func._name: self._wrap_function(func)})
-    
+
     def add_functions(self, func_list: List[Callable]):
         for func in func_list:
-            self.add_single_function(func['func'])
-
-
+            self.add_single_function(func["func"])
